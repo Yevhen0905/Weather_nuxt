fae835a44b8cf3bcff6615375c4a461a32eaccd1
Vittcenko <evgenav83@gmail.com>
2024-02-09 12:08:32 +0200

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,7 @@ export default defineNuxtConfig({
       link: [{ rel: "preconnect", href: "https://fonts.gstatic.com"}],
       link: [{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"}]
     },
+    baseURL: '/nuxt-github-pages/', 
+    buildAssetsDir: 'assets', 
   },
 });
